2f462f4d1135358a32b331f61944ed156961753e
kiplimoboor <kiplimoarapboor@gmail.com>
2024-08-21 20:29:02 +0300

    mru cache

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+"""A simple MRU Module"""
+
+from collections import OrderedDict
+
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """A simple MRU Cache implementation"""
+
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """
+        Sets a key in the cache dictionary, and removes the most recently used
+        if set cache limit is reached
+        Args:
+            key: the key to be set in the dictionary
+            item: the value to be set for the key
+        """
+        if not key or not item:
+            return
+
+        cache = self.cache_data
+
+        if len(cache) == self.MAX_ITEMS and key not in cache:
+            oldest, _ = cache.popitem(last=True)
+            print('DISCARD: ' + oldest)
+
+        cache[key] = item
+        cache.move_to_end(key)
+
+    def get(self, key):
+        """
+        Gets a key from the cache dictionary
+        Args:
+            key: the key to be accessed
+        Return:
+            the value linked to the key, None otherwise
+        """
+
+        if key in self.cache_data:
+            self.cache_data.move_to_end(key)
+        return self.cache_data.get(key)
