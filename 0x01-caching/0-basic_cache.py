cda8216e52dbe752c37022481f25848e5ea24624
kiplimoboor <kiplimoarapboor@gmail.com>
2024-08-21 11:00:20 +0300

    implement a cache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -4,37 +4,7 @@
 
 from functools import lru_cache
 
-
-class BaseCaching():
-    """ BaseCaching defines:
-      - constants of your caching system
-      - where your data are stored (in a dictionary)
-    """
-    MAX_ITEMS = 4
-
-    def __init__(self):
-        """ Initiliaze
-        """
-        self.cache_data = {}
-
-    def print_cache(self):
-        """ Print the cache
-        """
-        print("Current cache:")
-        for key in sorted(self.cache_data.keys()):
-            print("{}: {}".format(key, self.cache_data.get(key)))
-
-    def put(self, key, item):
-        """ Add an item in the cache
-        """
-        raise NotImplementedError(
-            "put must be implemented in your cache class")
-
-    def get(self, key):
-        """ Get an item by key
-        """
-        raise NotImplementedError(
-            "get must be implemented in your cache class")
+from base import BaseCaching
 
 
 @lru_cache(maxsize=None)
@@ -42,6 +12,12 @@ class BasicCache(BaseCaching):
     """
     A simple basic caching implementation
     """
+
+    def __init__(self):
+        """
+        Construct from the super class
+        """
+        super().__init__()
 
     def put(self, key, item):
         """
@@ -51,9 +27,8 @@ class BasicCache(BaseCaching):
            item: the value the key should hold
         """
 
-        if key is None or item is None:
-            return
-        self.cache_data[key] = item
+        if key and item:
+            self.cache_data[key] = item
 
     def get(self, key):
         """
